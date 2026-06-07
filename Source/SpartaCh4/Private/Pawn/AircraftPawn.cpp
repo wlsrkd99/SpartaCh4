347ec8cc95753dbae6a5f53935ee3fa9f783178a
kimjinkang <wlsrkd99@naver.com>
2026-06-08 00:33:48 +0900

    feat: 과제4 필수구현 완료

--- Source/SpartaCh4/Private/Pawn/AircraftPawn.cpp
+++ Source/SpartaCh4/Private/Pawn/AircraftPawn.cpp
@@ -3,32 +3,94 @@
 
 #include "Pawn/AircraftPawn.h"
 
-// Sets default values
+#include "EnhancedInputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Controller/SpartaPlayerController.h"
+#include "GameFramework/SpringArmComponent.h"
+
+
 AAircraftPawn::AAircraftPawn()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
-}
-
-// Called when the game starts or when spawned
-void AAircraftPawn::BeginPlay()
-{
-	Super::BeginPlay();
 	
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+	RootComponent = CapsuleComp;  
+	
+	SkeletalMeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMesh"));
+	SkeletalMeshComp->SetupAttachment(CapsuleComp);
+	
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArmComp->SetupAttachment(CapsuleComp);
+	SpringArmComp->SetRelativeLocation(FVector(0,0,40));
+	SpringArmComp->TargetArmLength = 300.0f;
+	SpringArmComp->bUsePawnControlRotation = false;
+	
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	CameraComp->SetupAttachment(SpringArmComp, USpringArmComponent::SocketName);
+	CameraComp->bUsePawnControlRotation = false;
+    
+	NormalSpeed = 600.0f;
 }
 
-// Called every frame
-void AAircraftPawn::Tick(float DeltaTime)
+void AAircraftPawn::Tick(float DeltaSeconds)
 {
-	Super::Tick(DeltaTime);
-
+	Super::Tick(DeltaSeconds);
+	
+	const FVector LocalOffset = FVector(MoveInput.X, MoveInput.Y, 0.f) * NormalSpeed * DeltaSeconds;
+	AddActorLocalOffset(LocalOffset, true);
 }
 
-// Called to bind functionality to input
 void AAircraftPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (ASpartaPlayerController* PlayerController = Cast<ASpartaPlayerController>(GetController()))
+		{
+			if (PlayerController->MoveAction)
+			{
+				EnhancedInput->BindAction(
+					PlayerController->MoveAction,
+					ETriggerEvent::Triggered,
+					this,
+					&AAircraftPawn::Move
+				);
+				EnhancedInput->BindAction(
+					PlayerController->MoveAction,
+					ETriggerEvent::Completed,
+					this,
+					&AAircraftPawn::MoveCompleted
+				);
+			}
+            
+			if (PlayerController->LookAction)
+			{
+				EnhancedInput->BindAction(
+					PlayerController->LookAction,
+					ETriggerEvent::Triggered,
+					this,
+					&AAircraftPawn::Look
+				);
+			}  
+		}
+	}
+}
 
+void AAircraftPawn::Move(const FInputActionValue& value)
+{
+	if (!Controller) return;
+	MoveInput = value.Get<FVector2d>();
+}
+
+void AAircraftPawn::MoveCompleted(const FInputActionValue& value)
+{
+	MoveInput = FVector2d::ZeroVector;
+}
+
+void AAircraftPawn::Look(const FInputActionValue& value)
+{
+	const FVector2D LookAxis = value.Get<FVector2D>();
+	AddActorLocalRotation(FRotator(LookAxis.Y, LookAxis.X, 0.f));
 }
 
--- Source/SpartaCh4/Public/Pawn/AircraftPawn.h
+++ Source/SpartaCh4/Public/Pawn/AircraftPawn.h
@@ -6,24 +6,44 @@
 #include "GameFramework/Pawn.h"
 #include "AircraftPawn.generated.h"
 
+class UCapsuleComponent;
+class USkeletalMeshComponent;
+class USpringArmComponent;
+class UCameraComponent;
+struct FInputActionValue;
+
 UCLASS()
 class SPARTACH4_API AAircraftPawn : public APawn
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
 	AAircraftPawn();
-
+	
+	virtual void Tick(float DeltaSeconds) override;
+	
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pawn|Movement")
+	float NormalSpeed;
+	
+private:
+	void Move(const FInputActionValue& value);
+	void MoveCompleted(const FInputActionValue& value);
+	void Look(const FInputActionValue& value);
+	
+	FVector2D MoveInput;
+	
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pawn|Camera")
+	UCapsuleComponent * CapsuleComp;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pawn|Camera")
+	USkeletalMeshComponent* SkeletalMeshComp;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pawn|Camera")
+	USpringArmComponent* SpringArmComp;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pawn|Camera")
+	UCameraComponent* CameraComp;
+	
+	
 };
